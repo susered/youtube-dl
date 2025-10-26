1fb4f2f58e5a112ff6c651f05c31d798559d2c90
ghu <github@pale-den.net>
2025-10-26 19:52:16 -0400

    Added class, VideoPostProcessingTools(), and method, compare_versions(), to compare python versions with a few attribute methods

--- youtube_dl/postprocessor/common.py
+++ youtube_dl/postprocessor/common.py
@@ -67,3 +67,68 @@ class PostProcessor(object):
 
 class AudioConversionError(PostProcessingError):
     pass
+
+class VideoPostProcessingTools(object):
+    """Video processing tools.
+    """
+    _downloader = None
+
+    def __init__(self, downloader=None):
+        """
+
+        """
+        self._downloader = downloader
+
+    @property
+    def downloader(self):
+        """
+        :return str
+        """
+        return self._downloader
+
+    @downloader.setter
+    def downloader(self, downloader):
+        """
+
+        :param downloader:
+        :return: None
+        """
+        self._downloader = downloader
+
+    def compare_versions(self, version_1, version_2):
+        """Determine if version_1 it greater than version_2 in the forms either
+        (major: int, minor: int, patch: int) or "major: int,minor: int,patch: int".
+
+        version_1: tuple
+        version_2: tuple
+        return: int : o means equal; -1 means version_1 < version_2; 1 means version_1 > version_2
+        """
+
+        try:
+            if isinstance(version_1, str):
+                version_1_tuple = tuple(map(int, version_1.split('.')))
+            elif isinstance(version_1, tuple) or isinstance(version_1, int):
+                version_1_tuple = tuple(map(int, version_1))
+            else:
+                raise TypeError
+
+            if isinstance(version_2, str):
+                version_2_tuple = tuple(map(int, version_2.split('.')))
+            elif isinstance(version_2, tuple) or isinstance(version_2, int):
+                version_2_tuple = tuple(map(int, version_2))
+            else:
+                raise TypeError
+
+            for version1, version2 in zip(version_1_tuple, version_2_tuple):
+                if version1 > version2:
+                    return 1
+
+                if version1 < version2:
+                    return -1
+
+            return 0
+
+        except ValueError as ve:
+            self._downloader.report_warning(ve)
+        except TypeError as te:
+            self._downloader.report_warning(te)
